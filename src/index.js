cd4607ca1305f794cbb86d7f816ed05aa5f6cc11
alexum <alexum2013@yandex.ru>
2022-11-10 20:29:39 +0300

    main js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = "Проверка";
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+import { createRoot } from 'react-dom/client';
+import App from "./components/App";
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
